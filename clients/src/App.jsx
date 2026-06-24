6e4d09eb78e35caf57e4be9f2766d8a1f4efbc0b
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-06-24 17:34:25 +0530

    Created bascis file and some feature

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import Sidebar from "./Components/Sidebar";
+import { Route, Routes } from "react-router";
+import ChatBox from "./Components/ChatBox";
+import Credits from "./Pages/Credits";
+import Community from "./Pages/Community";
 
 const App = () => {
   return (
-    <div>
-      <div>Hello World</div>
-    </div>
+    <>
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 };
 
