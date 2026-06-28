0ce9a6387a7295f0bf98e94ff0738706f36d75b5
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-06-28 13:13:47 +0530

    Create user model and connect database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,19 @@
 import express from "express";
-const app = express();
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
 
-app.listen(3000, () => {
-  console.log("Server is started");
+const app = express();
+await connectDB();
+
+const PORT = process.env.PORT;
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
