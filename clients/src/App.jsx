8cb172f3f22a82c9d6bf6889c27407a47029fded
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-06-28 09:32:08 +0530

    Create login and signup pages

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -5,10 +5,13 @@ import ChatBox from "./Components/ChatBox";
 import Credits from "./Pages/Credits";
 import Community from "./Pages/Community";
 import { assets } from "./assets/assets";
+import Login from "./Pages/Login";
 import "./assets/prism.css";
 import Loading from "./Pages/Loading";
+import { useAppContext } from "./Context/AppContext";
 
 const App = () => {
+  const { user } = useAppContext();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { pathname } = useLocation();
   if (pathname === "/loading") return <Loading />;
@@ -21,16 +24,23 @@ const App = () => {
           onClick={() => setIsMenuOpen(true)}
         />
       )}
-      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
-        <div className="flex h-screen w-screen">
-          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-          <Routes>
-            <Route path="/" element={<ChatBox />} />
-            <Route path="/credits" element={<Credits />} />
-            <Route path="/community" element={<Community />} />
-          </Routes>
+
+      {user ? (
+        <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+          <div className="flex h-screen w-screen">
+            <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path="/" element={<ChatBox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="bg-gradient-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen">
+          <Login />
+        </div>
+      )}
     </>
   );
 };
